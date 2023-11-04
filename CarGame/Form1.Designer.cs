180eeacbdff3346ba048ed43e807a02086d471c6
ArmHos <arh20025@gmail.com>
2023-11-04 21:16:07 +0400

    initial commit

---
+++ CarGame/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.award = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.Enemy1 = new System.Windows.Forms.PictureBox();
+            this.Enemy2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.roadTruck2 = new System.Windows.Forms.PictureBox();
+            this.roadTrack1 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.award)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTruck2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.award);
+            this.panel1.Controls.Add(this.explosion);
+            this.panel1.Controls.Add(this.Enemy1);
+            this.panel1.Controls.Add(this.Enemy2);
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.roadTruck2);
+            this.panel1.Controls.Add(this.roadTrack1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 520);
+            this.panel1.TabIndex = 0;
+            // 
+            // award
+            // 
+            this.award.Image = ((System.Drawing.Image)(resources.GetObject("award.Image")));
+            this.award.Location = new System.Drawing.Point(107, 189);
+            this.award.Name = "award";
+            this.award.Size = new System.Drawing.Size(250, 100);
+            this.award.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.award.TabIndex = 7;
+            this.award.TabStop = false;
+            // 
+            // explosion
+            // 
+            this.explosion.Image = ((System.Drawing.Image)(resources.GetObject("explosion.Image")));
+            this.explosion.Location = new System.Drawing.Point(65, 295);
+            this.explosion.Name = "explosion";
+            this.explosion.Size = new System.Drawing.Size(64, 64);
+            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.explosion.TabIndex = 6;
+            this.explosion.TabStop = false;
+            // 
+            // Enemy1
+            // 
+            this.Enemy1.Image = global::CarGame.Properties.Resources.car2;
+            this.Enemy1.Location = new System.Drawing.Point(65, 106);
+            this.Enemy1.Name = "Enemy1";
+            this.Enemy1.Size = new System.Drawing.Size(50, 101);
+            this.Enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Enemy1.TabIndex = 5;
+            this.Enemy1.TabStop = false;
+            this.Enemy1.Tag = "carLeft";
+            // 
+            // Enemy2
+            // 
+            this.Enemy2.Image = global::CarGame.Properties.Resources.car4;
+            this.Enemy2.Location = new System.Drawing.Point(307, 93);
+            this.Enemy2.Name = "Enemy2";
+            this.Enemy2.Size = new System.Drawing.Size(50, 100);
+            this.Enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Enemy2.TabIndex = 4;
+            this.Enemy2.TabStop = false;
+            this.Enemy2.Tag = "carRight";
+            // 
+            // player
+            // 
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(268, 377);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // roadTruck2
+            // 
+            this.roadTruck2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roadTruck2.BackgroundImage")));
+            this.roadTruck2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.roadTruck2.ErrorImage = null;
+            this.roadTruck2.ImageLocation = "";
+            this.roadTruck2.Location = new System.Drawing.Point(0, 0);
+            this.roadTruck2.Name = "roadTruck2";
+            this.roadTruck2.Size = new System.Drawing.Size(550, 520);
+            this.roadTruck2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.roadTruck2.TabIndex = 1;
+            this.roadTruck2.TabStop = false;
+            // 
+            // roadTrack1
+            // 
+            this.roadTrack1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("roadTrack1.BackgroundImage")));
+            this.roadTrack1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.roadTrack1.ErrorImage = null;
+            this.roadTrack1.ImageLocation = "";
+            this.roadTrack1.Location = new System.Drawing.Point(0, -436);
+            this.roadTrack1.Name = "roadTrack1";
+            this.roadTrack1.Size = new System.Drawing.Size(550, 520);
+            this.roadTrack1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadTrack1.TabIndex = 0;
+            this.roadTrack1.TabStop = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(229, 561);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(101, 32);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Restart";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.restartGame);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Bahnschrift", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(183, 605);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(154, 45);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(572, 694);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(590, 741);
+            this.Name = "Form1";
+            this.Text = "CarGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.award)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTruck2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox roadTrack1;
+        private System.Windows.Forms.PictureBox roadTruck2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox award;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.PictureBox Enemy1;
+        private System.Windows.Forms.PictureBox Enemy2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label txtScore;
+    }
+}
+
---
+++ CarGame/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CarGame
+{
+    public partial class Form1 : Form
+    {
+        int roadSpeed = 15;
+        int trafficSpeed = 18;
+        int playerSpeed = 15;
+        int score = 0;
+        int carImage;
+
+        Bitmap[] images = {
+            Properties.Resources.car1,
+            Properties.Resources.car2,
+            Properties.Resources.car3,
+            Properties.Resources.car4,
+            Properties.Resources.car5,
+            Properties.Resources.car6,
+            Properties.Resources.car7,
+            Properties.Resources.car8,
+        };
+
+        Random rand = new Random();
+        Random carPosition = new Random();
+
+        bool goLeft, goRight, goUp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+        }
+        
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+
+        {
+            incrementScore();
+            playerMovesHandler();
+            roadParallaxMovement();
+            enemieMovesHandler();
+            checkCollision();
+            winAward();
+        }
+
+        private void changeEnemyCars(PictureBox tempCar)
+        {
+            carImage = rand.Next(1, 8);
+            tempCar.Image = images[carImage];
+
+            tempCar.Top = carPosition.Next(100, 400) * -1;
+
+            if((string)tempCar.Tag == "carLeft")
+            {
+                Enemy1.Left = carPosition.Next(12, 234);
+            }
+            if ((string)tempCar.Tag == "carRight")
+            {
+                Enemy2.Left = carPosition.Next(306, 360);
+            }
+        }
+
+        private void gameOver()
+        {
+            playSound();
+            gameTimer.Stop();
+            explosion.Visible = true;
+            player.Controls.Add(explosion);
+            explosion.Location = new Point(-8, 5);
+            explosion.BackColor = Color.Transparent;
+
+            award.Visible = true;
+            award.BringToFront();
+            startButton.Enabled = true;
+        }
+
+        private void resetGame()
+        {
+            startButton.Enabled = false;
+            explosion.Visible = false;
+            award.Visible= false;
+            goLeft = false; 
+            goRight = false ;
+            score = 0;
+
+            Image originalImage = Properties.Resources.lose1;
+            int newWidth = 250;
+            int newHeight = 100;
+            Bitmap resizedImage = new Bitmap(originalImage, newWidth, newHeight);
+
+            award.Image = resizedImage;
+
+            Enemy1.Top = carPosition.Next(20, 450) * -1;
+            Enemy1.Left = carPosition.Next(12, 234);
+
+            Enemy2.Top = carPosition.Next(200, 450) * -1;
+            Enemy2.Left = carPosition.Next(306, 360);
+
+            gameTimer.Start();
+        }
+        
+        private void restartGame(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+
+        private void playSound()
+        {
+            System.Media.SoundPlayer playCrash = new System.Media.SoundPlayer(Properties.Resources.hit);
+            playCrash.Play();
+        }
+
+        private void winAward()
+        {
+            if (score > 100 && score < 500)
+            {
+                award.Image = Properties.Resources.bronze;
+                roadSpeed = 15;
+                trafficSpeed = 20;
+            }
+            else if (score > 500 && score < 1000)
+            {
+                award.Image = Properties.Resources.silver;
+                roadSpeed = 23;
+                trafficSpeed = 26;
+            }
+            else if (score > 1000)
+            {
+                award.Image = Properties.Resources.gold;
+                roadSpeed = 27;
+                trafficSpeed = 32;
+            }
+        }
+
+        private void enemieMovesHandler()
+        {
+            Enemy1.Top += trafficSpeed;
+            Enemy2.Top += trafficSpeed;
+
+            if (Enemy1.Top > 520)
+            {
+                changeEnemyCars(Enemy1);
+            }
+            if (Enemy2.Top > 520)
+            {
+                changeEnemyCars(Enemy2);
+            }
+        }
+        private void roadParallaxMovement()
+        {
+            roadTrack1.Top += roadSpeed;
+            roadTruck2.Top += roadSpeed;
+
+            if (roadTruck2.Top > 425)
+            {
+                roadTruck2.Top = -425;
+            }
+            if (roadTrack1.Top > 425)
+            {
+                roadTrack1.Top = -425;
+            }
+        }
+
+        private void playerMovesHandler()
+        {
+            if (goLeft == true && player.Left > 5)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true && player.Left < 350)
+            {
+                player.Left += playerSpeed;
+            }
+
+            if (goUp == true)
+            {
+                gameTimer.Interval = 15;
+            }
+            else
+            {
+                gameTimer.Interval = 20;
+            }
+
+        }
+
+        private void checkCollision()
+        {
+            if (player.Bounds.IntersectsWith(Enemy1.Bounds) || player.Bounds.IntersectsWith(Enemy2.Bounds))
+            {
+                gameOver();
+            }
+        }
+
+        private void incrementScore()
+        {
+            txtScore.Text = $"Score: {score}";
+            score++;
+        }
+    }
+}
